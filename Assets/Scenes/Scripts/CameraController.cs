1a3980249740766821b94e3b1cd3737159b519a0
rimayu0815 <rikussyuu@gmail.com>
2020-12-15 22:31:45 +0900

    1.新しいプロジェクトを作成 2.ゲームシーンを作成して、ゲームの画面の設定を行う 3.アセットをUnity Asset Storeダウンロードしてインポートする 4.アセットを使ってステージを作成する 5.アセットを使ってプレイヤー用のキャラを設置する 6.キャラを自然落下させて水面に落下した際に停止させる 7.スクリプトを使って、カメラがキャラを追従するように制御する

---
+++ Assets/Scenes/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject playerObj;
+
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - playerObj.transform.position;//カメラと追随対象のゲームオブジェクト（ペンギン等）との距離（初期位置）を補正値として取得
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerObj !=null)
+        {
+            transform.position = playerObj.transform.position + offset;//カメラの位置を追随対象の位置＋補正値にする
+        }
+    }
+}
